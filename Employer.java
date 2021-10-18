3390229c5ffaade44040f513e0e825f80836d5c5
TylerMadden <tyler.madden@outlook.com>
2021-10-17 22:25:48 -0400

    Created and skeleton filled files

---
+++ Employer.java
@@ -0,0 +1,39 @@
+public class Employer extends WorkforceMember{
+    String username;
+    String password;
+    String email;
+    String firstName;
+    String lastName;
+    String location;
+    String company;
+
+    public Employer(String username, String password, String email, String firstName, String lastName, String location, String company){
+        this.username = username;
+        this.password = password;
+        this.email = email;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.location = location;
+        this.company = company;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getFirstName(){
+        return firstName;
+    }
+
+    public String getLastName(){
+        return lastName;
+    }
+
+    public String getLocation(){
+        return location;
+    }
+
+    public String getCompany(){
+        return company;
+    }
+}
---
+++ InternshipApplication.java
@@ -0,0 +1,49 @@
+public class InternshipApplication {
+    JobList jobListing;
+    UserList userList;
+    User user;
+
+    public InternshipApplication(){
+
+    }
+
+    public ArrayList<JobListing> findJobs(){
+
+    }
+
+    public User createAccount(String username, String password, String email, String firstName, String lastName){
+
+    }
+
+    public User login(String userName, String password){
+
+    }
+
+    public JobListing addJobListing(float pay, String length, String position, String jobDescription, ArrayList<String> skills){
+
+    }
+
+    public Rating addRating(int numRating, String comment, WorkforceMember member){
+
+    }
+
+    public Rating editRating(Rating rating, int numRating, String comment, WorkforceMember member){
+
+    }
+
+    public Rating deleteRating(Rating rating){
+
+    }
+
+    public JobListing editListing(JobListing job, float pay, String length, String position, String jobDescription, ArrayList<String> skills){
+
+    }
+
+    public JobListing deleteJobListing(JobListing job){
+
+    }
+
+    public void Apply(JobListing job){
+        
+    }
+}
