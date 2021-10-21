7d64319dbd5511c4de47294e623c9c7fccc982dc
Patbu13 <70173620+Patbu13@users.noreply.github.com>
2021-10-21 15:51:50 -0400

    RatingList made

--- DataLoader.java
+++ DataLoader.java
@@ -1,10 +1,29 @@
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.util.Scanner;
+
 public class DataLoader {
+
+    // relative path to the file, based on the directory you currently have open
+    private static final String ADMIN_FILE = "Internship-builder\\admin.json";
+    private static final String EDUCATION_FILE = "Internship-builder\\education.json";
+    private static final String EMPLOYER_FILE = "Internship-builder\\employer.json";
+    private static final String EXPERIENCE_FILE = "Internship-builder\\experience.json";
+    private static final String JOBLISTING_FILE = "Internship-builder\\jobListing.json";
+    private static final String RATING_FILE = "Internship-builder\\rating.json";
+    private static final String RESUME_FILE = "Internship-builder\\resume.json";
+    private static final String STUDENT_FILE = "Internship-builder\\student.json";
+
     public static ArrayList<User> getUsers() {
         return null;
     }
 
     public static ArrayList<JobListing> getJobListing() {
+        return null;
+    }
+
+    public static ArrayList<Rating> getRating() {
         return null;
     }
 }
