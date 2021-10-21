54cfdc39cd5ccacdd839b0d6e1f657491f0bc99d
Patrick Burroughs <burrougp@email.sc.edu>
2021-10-21 14:22:22 -0400

    get rekt

--- Rating.java
+++ Rating.java
@@ -3,15 +3,8 @@ import java.util.ArrayList;
 public class Rating {
     private int numRating;
     private String comment;
-<<<<<<< HEAD
     private User user;
     private ArrayList<Rating> ratings;
-=======
-    private User member;
-    private ArrayList<Rating> rating;
-
-    public Rating(int numRating, String comment, User member) {
->>>>>>> 1255c95f006c8401997389fad7c3c114d8e9353f
 
     public Rating(int numRating, String comment, User user) {
         this.numRating = numRating;
@@ -32,11 +25,7 @@ public class Rating {
         return 0;
     }
 
-<<<<<<< HEAD
     public User getUser() {
-=======
-    public User getMember() {
->>>>>>> 1255c95f006c8401997389fad7c3c114d8e9353f
         return null;
     }
 }
